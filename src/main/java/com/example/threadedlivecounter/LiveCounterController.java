ed2f2d6342be09a9f45dd5a69e7dddd0a52d032a
Asmaul Hosna Nishat <nishatsoyati@gmail.com>
2025-06-06 08:33:10 +0600

    first commit

---
+++ src/main/java/com/example/threadedlivecounter/LiveCounterController.java
@@ -0,0 +1,14 @@
+package com.example.threadedlivecounter;
+
+import javafx.event.ActionEvent;
+
+public class LiveCounterController
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void submitButtonOnAction(ActionEvent actionEvent) {
+    }
+}
