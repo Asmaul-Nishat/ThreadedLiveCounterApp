1027a4e0ad5b68f6514d54ef520023120f5c0be6
Asmaul Hosna Nishat <nishatsoyati@gmail.com>
2025-06-06 08:57:45 +0600

    commit

--- src/main/java/com/example/threadedlivecounter/LiveCounterController.java
+++ src/main/java/com/example/threadedlivecounter/LiveCounterController.java
@@ -1,9 +1,16 @@
 package com.example.threadedlivecounter;
 
 import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class LiveCounterController
 {
+    @javafx.fxml.FXML
+    private Label updateCounterTextField;
+    @javafx.fxml.FXML
+    private TextField inputTextField;
+
     @javafx.fxml.FXML
     public void initialize() {
     }
